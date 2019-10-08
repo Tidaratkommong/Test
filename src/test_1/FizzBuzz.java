486eaa09c4d6b1ba6e85f7a48b94c0eb0cef7b85
nitimaporn <nitimaporn.bai@gmail.com>
2019-10-09 00:13:52 +0700

    test1_input1

---
+++ src/test_1/FizzBuzz.java
@@ -0,0 +1,9 @@
+package test_1;
+
+public class FizzBuzz {
+	public int printStringinput(int number) {
+		// TODO Auto-generated method stub
+		return number;
+	}
+}
+
---
+++ src/test_1/testfizzbuzz.java
@@ -0,0 +1,17 @@
+package test_1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+	@Test
+	void test1_input1() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		int input = fizzbuzz.printStringinput(1);
+		assertEquals(1, input);
+		
+	}
+}
+
